0622f11c15eaa30fb3050fdda788fd4ef164525d
Abdi-01 <abdialghi@gmail.com>
2025-06-25 22:18:53 +0700

    revamp multer cloudinary

--- src/middleware/uploader.ts
+++ src/middleware/uploader.ts
@@ -7,9 +7,7 @@ export const uploaderMemory = () => {
     limits: { fileSize: 1 * 1024 * 1024 },
     fileFilter: (req: Request, file: Express.Multer.File, callback) => {
       const allowedExt = /\.(jpeg|jpg|png|gif)$/;
-      const existingName = file.originalname.split(".");
-      const ext = existingName[existingName.length - 1].toLowerCase();
-      if (!allowedExt.test(ext)) {
+      if (!allowedExt.test(file.originalname.toLowerCase())) {
         return callback(
           new Error("Wrong file extention only (jpeg|jpg|png|gif)")
         );
