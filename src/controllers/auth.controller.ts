7856092ba93bbad72bec69efcf3eab27d3dfe961
Abdi-01 <abdialghi@gmail.com>
2025-06-25 19:15:33 +0700

    auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,4 +1,8 @@
 import { NextFunction, Request, Response } from "express";
+import { prisma } from "../config/prisma";
+import { hashPassword } from "../utils/hash";
+import { compare } from "bcrypt";
+import { sign } from "jsonwebtoken";
 
 class AuthController {
   public async register(
@@ -6,7 +10,32 @@ class AuthController {
     res: Response,
     next: NextFunction
   ): Promise<void> {
-    // code
+    try {
+      const existingUser = await prisma.user.findUnique({
+        where: {
+          email: req.body.email,
+        },
+      });
+
+      if (existingUser) {
+        throw { rc: 400, success: false, message: "User already exist" };
+      }
+
+      const newUser = await prisma.user.create({
+        data: {
+          ...req.body,
+          password: await hashPassword(req.body.password),
+          role: req.body.role || "author",
+        },
+      });
+
+      res.status(201).send({
+        success: true,
+        message: "User register success",
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 
   public async login(
@@ -14,7 +43,38 @@ class AuthController {
     res: Response,
     next: NextFunction
   ): Promise<void> {
-    // code
+    try {
+      const findUser = await prisma.user.findUnique({
+        where: {
+          email: req.body.email,
+        },
+      });
+
+      if (!findUser) {
+        throw { rc: 404, message: "User not exist" };
+      }
+
+      const comparePass = await compare(req.body.password, findUser.password);
+      if (!comparePass) {
+        throw { rc: 401, message: "Password is wrong" };
+      }
+
+      const token = sign(
+        { id: findUser.id, role: findUser.role },
+        process.env.TOKEN_KEY || "secret",
+        { expiresIn: "1h" }
+      );
+
+      res.status(200).send({
+        username: findUser.username,
+        email: findUser.email,
+        imgProfile: findUser.imgProfile,
+        role: findUser.role,
+        token,
+      });
+    } catch (error) {
+      next(error);
+    }
   }
 }
 
